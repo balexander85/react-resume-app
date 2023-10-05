07ab0a00ce3bf935bd2037308d253ec4c900a91c
Brian A <brian@dadgumsalsa.com>
2023-10-04 21:16:06 -0500

    Moving resume data to json file

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import Summary from './Summary';
 import JobHistory from './JobHistory';
 import EducationSX from './Education';
 import Skills from "./Skills";
-import {summaryText, jobHistory, educationHistory, skillsList, contactInfo} from './tempConstants';
+import candidate from './candidate.json';
 import ContactSX from "./Contact";
 
 function App() {
   return (
     <div className="App">
-      <ContactSX contact={contactInfo} />
-      <Summary summaryList={summaryText} />
-      <JobHistory jobs={jobHistory} />
-      <EducationSX educationList={educationHistory} />
-      <Skills skillsList={skillsList} />
+      <ContactSX contact={candidate.contact} />
+      <Summary summaryList={candidate.summary} />
+      <JobHistory jobs={candidate.history} />
+      <EducationSX educationList={candidate.education} />
+      <Skills skillsList={candidate.skills} />
     </div>
   );
 }
