8295a9f91c04f39af62ab015fa7ac38a35d355ae
Brian A <brian@dadgumsalsa.com>
2023-10-10 20:38:54 -0500

    Improving Contact component and title element

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -9,11 +9,17 @@ interface ContactProps {
     contact: Contact;
 }
 
+const defaultName =  "Default Name"
+const defaultLocation = "Default Location";
+
 const ContactSX: React.FC<ContactProps> = ({ contact }) => {
+    const contactName = contact.name ? contact.name : defaultName;
+    const contactLocation = contact.location ? contact.location : defaultLocation;
+    document.title = `${contactName} | ${contactLocation}`;
     return (
         <section className="contact-info">
-            <div className="contact-name">{contact.name ? contact.name : "Default Name"}</div>
-            <div className="contact-location">{contact.location ? contact.location : "Default Location"}</div>
+            <div className="contact-name">{contactName}</div>
+            <div className="contact-location">{contactLocation}</div>
             {contact.linkedIn &&
                 (<div className="contact-linkedin">
                     <a href={contact.linkedIn} target="_blank" rel="noreferrer">
